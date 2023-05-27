133191051ee5a0d08dc915aada16f44cc9c1bbd9
wqiy <yjsswqy@qq.com>
2023-05-27 15:50:42 +0800

    feat: Dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the home handler so it is defined as a method againset 
+// application
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -25,7 +26,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// templates in a template set.
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Printf(err.Error())
+		// home handler now can access application's fields
+		app.errorLog.Printf(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
@@ -33,13 +35,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// Use the ExecuteTemplate() method to write the content of the "base"
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Printf(err.Error())
+		app.errorLog.Printf(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -48,7 +50,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// check method is post
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,13 @@ import (
 	"os"
 )
 
+// Define an application struct to hold the application-wide dependencies 
+// for the web application.
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
+
 func main() {
 	// Define a new command-line flag
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -20,6 +27,12 @@ func main() {
 	// Create a logger for writing error messages. 
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	// Initialize a new instance of application struct.
+	app := application {
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
 	mux := http.NewServeMux()
 
 	// Create a file server which serves files out of the "./ui/static" directory.
@@ -29,9 +42,9 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Register the other application routes as normal.
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	// Initialize a new http.Server struct.
 	srv := &http.Server{
